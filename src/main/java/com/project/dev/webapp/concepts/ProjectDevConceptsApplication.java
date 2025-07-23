9ee7c5f0b6ebfa263b9005de744d43718f9a1cfb
Ashish Gautam <ashishgautam9560@gmail.com>
2025-07-23 21:08:36 +0530

    Initial commit

---
+++ src/main/java/com/project/dev/webapp/concepts/ProjectDevConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.project.dev.webapp.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDevConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dev/webapp/concepts/ProjectDevConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dev.webapp.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDevConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
