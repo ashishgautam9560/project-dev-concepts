ef1614d16e92f12fc945724dc658208954bd6247
Ashish Gautam <ashishgautam9560@gmail.com>
2025-07-28 23:07:58 +0530

    added RestTemplate changes

---
+++ src/main/java/com/project/dev/webapp/concepts/config/RestTemplateConnectionPoolConfig.java
@@ -0,0 +1,54 @@
+package com.project.dev.webapp.concepts.config;
+
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConnectionPoolConfig {
+
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+		// Create HttpClient Connection Pool with maximum TCP connection as 120 and maximum TCP/service as 30
+		PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+		poolingHttpClientConnectionManager.setMaxTotal(120);
+		poolingHttpClientConnectionManager.setDefaultMaxPerRoute(30);
+		
+
+		// Connection Request Timeout - how long to wait to get a connection from the connection pool.
+		// Connect Timeout - Time to establish a TCP connection to the remote host.
+		// Response Timeout - Time to wait for a server response after sending the request.
+		RequestConfig requestConfig = RequestConfig
+										.custom()
+										.setConnectionRequestTimeout(Timeout.ofSeconds(300))
+										.setConnectTimeout(Timeout.ofSeconds(300))
+										.setResponseTimeout(Timeout.ofSeconds(300))
+										.build();
+
+		
+		// Builds an **HttpClient (TCP Connection)** with the pooling and timeout configuration.
+		CloseableHttpClient closeableHttpClient = HttpClientBuilder
+													.create()
+													.setConnectionManager(poolingHttpClientConnectionManager)
+													.setDefaultRequestConfig(requestConfig)
+													.build();
+
+		
+		// Wraps the custom HttpClient into a Spring-compatible ClientHttpRequestFactory.
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = 
+				new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
+
+		
+		// Creates the RestTemplate Bean
+		return new RestTemplate(httpComponentsClientHttpRequestFactory);
+	}
+
+}
